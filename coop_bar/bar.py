e4ca82fee78c307cb2c2ab9ead468089181d3c85
Dominique Guardiola <dguardiola@quinode.fr>
2012-09-17 18:00:13 +0200

    get_footer method and a tribute to Jimmy Cliff

--- coop_bar/bar.py
+++ coop_bar/bar.py
@@ -4,15 +4,16 @@ from django.utils.importlib import import_module
 from django.conf import settings
 
 class CoopBar:
-    __we_are_all_one = {}
-    
+    __we_all_are_one = {}
+
     def __init__(self):
-        self.__dict__ = self.__we_are_all_one #Borg pattern
-        
+        self.__dict__ = self.__we_all_are_one #Borg pattern
+
         if not self.__dict__: #Don't reload for each instance
             self._callbacks = []
             self._headers = []
-            
+            self._footer = []
+
             if hasattr(settings, 'COOP_BAR_MODULES'):
                 for module_name in settings.COOPBAR_MODULES:
                     try:
@@ -20,7 +21,7 @@ class CoopBar:
                         loader_fct = getattr(app_admin_bar_module, 'load_commands')
                         loader_fct(self)
                     except ImportError:
-                        raise ImportError(u"coop_bar : error while loading '{0}'. Check COOPBAR_MODULES in settings".format(module_name)) 
+                        raise ImportError(u"coop_bar : error while loading '{0}'. Check COOPBAR_MODULES in settings".format(module_name))
             else:
                 for app in settings.INSTALLED_APPS:
                     try:
@@ -34,22 +35,25 @@ class CoopBar:
                             loader_fct(self)
                     except ImportError:
                         pass
-        
+
     def register_header(self, callback):
         self._headers.append(callback)
-        
+
+    def register_footer(self, callback):
+        self._footer.append(callback)
+
     def register_command(self, callback):
         self._callbacks.append(callback)
-        
+
     def register_separator(self):
         self._callbacks.append(None)
-        
+
     def register(self, list_of_list_of_cmds):
         for list_of_cmds in list_of_list_of_cmds:
             for callback in list_of_cmds:
                 self.register_command(callback)
             self.register_separator()
-                
+
     def get_commands(self, request, context):
         commands = []
         separator = '<div class="separator"></div>'
@@ -66,10 +70,10 @@ class CoopBar:
                 html = c(request, context)
             if html:
                 commands.append(html)
-        if commands and commands[-1]==separator:
+        if commands and commands[-1] == separator:
             commands = commands[:-1]
         return commands
-    
+
     def get_headers(self, request, context):
         headers = []
         for c in self._headers:
@@ -77,4 +81,12 @@ class CoopBar:
             if html:
                 headers.append(html)
         return headers
+
+    def get_footer(self, request, context):
+        footer = []
+        for c in self._footer:
+            html = c(request, context)
+            if html:
+                footer.append(html)
+        return footer
 
